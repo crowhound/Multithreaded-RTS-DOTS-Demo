8cc3d56d7b7aac3238f3eaed594347f9689db4fa
crowhound <jonathan.westfall1@gmail.com>
2024-11-19 14:37:36 -0600

    Added the ability to select multiple Units with left mouse button dragging.
    Added:
    - Created a helper function to create Rect for SelectionAreaRect for selecting Units
    - Added a function to update the UI image selection visual indicator for the selection rect area.
    
    Changed:
    - Updated the left mouse button up section in UnitSelectionManager to allow for creating a selection area that filters out non-unit entities to allow for selecting units only.

--- Assets/Scripts/Managers/UnitSelectionManager.cs
+++ Assets/Scripts/Managers/UnitSelectionManager.cs
@@ -4,6 +4,8 @@ using SF.EntitiesModule;
 
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Physics;
+using Unity.Transforms;
 
 using UnityEngine;
 
@@ -30,13 +32,78 @@ namespace SF.EntitiesModule
             {
                 // Get the screen coordinate of the mouse position.
                 _selectionStartMousePosition = Input.mousePosition;
+                OnSelectionAreaStart?.Invoke(this,EventArgs.Empty);
             }
 
 
             if(Input.GetMouseButtonUp(0))
             {
                 // Get the screen coordinate of the mouse position.
-                Vector2 selectionStartMousePosition = Input.mousePosition;
+
+                EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+                // Make a query to get the entity units in a system.
+                // We set the allocator to temp so we don't keep the allocation in memory.
+
+                // Get a query of all Units already selected so we can cycle and deselect them.
+                EntityQuery entityQuery = new EntityQueryBuilder(Allocator.Temp)
+                                                .WithAll<Selected>()
+                                                .Build(entityManager);
+
+                // Get the native array of entities from the query
+                NativeArray<Entity> entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+
+                // Deselect all units that were previously selected.
+                for(int i = 0; i < entityArray.Length; i++)
+                {                             
+                    entityManager.SetComponentEnabled<Selected>(entityArray[i], false);
+                }
+
+
+                Rect selectionAreaRect = GetSelectionAreaRect();
+                float selectionAreaSize = selectionAreaRect.width + selectionAreaRect.height;
+                float multipleSelectionSizeMin = 40f;
+                bool isMultipleSelection = selectionAreaSize > multipleSelectionSizeMin;
+
+                if(isMultipleSelection) // Multiple Selection
+                {
+                    // First we get all Entities with a LocalTransform and Unit IComponentData struct attached.
+                    // Than we make sure we only select Units that are selectable. 
+                    // Note WithPresent method makes sure they have that IComponentData struct, but it will allow for disabled and enabled Selected IComponentData. 
+                    entityQuery = new EntityQueryBuilder(Allocator.Temp)
+                                                    .WithAll<LocalTransform, Unit>()
+                                                    .WithPresent<Selected>()
+                                                    .Build(entityManager);
+
+                    entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+
+                    // Get the native array of our units LocalTransform IDataComponents.
+                    NativeArray<LocalTransform> localTransformArray = entityQuery.ToComponentDataArray<LocalTransform>(Allocator.Temp);
+
+                    // Remember IComponentData are normally structs so
+                    // You can't just modify the value. You have to copy back the new data into the entity array.
+                    for(int i = 0; i < localTransformArray.Length; i++)
+                    {
+                        // Get a copy of the structs current value
+                        LocalTransform unitLocalTransform = localTransformArray[i];
+
+                        // Get the screen position of the unit being 
+                        Vector2 unitScreenPosition = Camera.main.WorldToScreenPoint(unitLocalTransform.Position);
+
+                        // Does our selection area rect contain the unit
+                        if(selectionAreaRect.Contains(unitScreenPosition))
+                        {
+                            // Unit is inside the selected Unit area.
+
+                            // Set the units in the selection as selected.
+                            entityManager.SetComponentEnabled<Selected>(entityArray[i], true);
+                        }
+                    }
+
+                }
+
+
+                OnSelectionAreaEnd?.Invoke(this, EventArgs.Empty);
             }
 
             if(Input.GetMouseButtonDown(1))
@@ -78,5 +145,27 @@ namespace SF.EntitiesModule
                 entityQuery.CopyFromComponentDataArray(unitMoverArray);
             }
         }
+
+        public Rect GetSelectionAreaRect()
+        {
+            Vector2 selectionEndMousePosition = Input.mousePosition;
+
+            Vector2 lowerLeftCorner = new Vector2(
+                Mathf.Min(_selectionStartMousePosition.x, selectionEndMousePosition.x),
+                Mathf.Min(_selectionStartMousePosition.y, selectionEndMousePosition.y)
+            );
+
+            Vector2 upperRightCorner = new Vector2(
+               Mathf.Max(_selectionStartMousePosition.x, selectionEndMousePosition.x),
+               Mathf.Max(_selectionStartMousePosition.y, selectionEndMousePosition.y)
+           );
+            return new Rect(
+                lowerLeftCorner.x,
+                lowerLeftCorner.y,
+                upperRightCorner.x - lowerLeftCorner.x,
+                upperRightCorner.y - lowerLeftCorner.y
+            );
+        }
     }
+
 }
--- Assets/Scripts/UI/UnitSelectionManagerUI.cs
+++ Assets/Scripts/UI/UnitSelectionManagerUI.cs
@@ -3,27 +3,70 @@ using System;
 using SF.EntitiesModule;
 
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SF.UI
 {
     public class UnitSelectionManagerUI : MonoBehaviour
     {
+        /// <summary>
+        /// This is the Rect transform of the image component itself. 
+        /// Make sure this is the correct UI object with the image component on it.
+        /// </summary>
         [SerializeField] private RectTransform _selectionAreaRectTransform;
 
         private void Start()
         {
             UnitSelectionManager.Instance.OnSelectionAreaStart += UnitSelectionManager_OnSelectionAreaStart;
             UnitSelectionManager.Instance.OnSelectionAreaEnd += UnitSelectionManager_OnSelectionAreaEnd;
+
+            // Make sure we are getting the correct RectTransform. If not, log an error.
+            if(_selectionAreaRectTransform == null || !_selectionAreaRectTransform.TryGetComponent(out Image selectionImage))
+            {
+                Debug.LogError("The RectTransform is either null or the _selectionAreaRectTransform is assigned to it doesn't have an image component. The UnitSelectionManagerUI functionality expects the _selectionAreaRectTransform to have an image component.", gameObject);
+                return;
+            }
+
+            // Make the selection UI disabled by default.
+            _selectionAreaRectTransform.gameObject.SetActive(false);
         }
 
+        private void Update()
+        {
+            if(_selectionAreaRectTransform != null && _selectionAreaRectTransform.gameObject.activeSelf)
+            {
+                UpdateVisual();
+            }
+
+        }
         private void UnitSelectionManager_OnSelectionAreaStart(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            if(_selectionAreaRectTransform == null)
+                return;
+
+            // Enable the visual UI selector image when starting a drag selection.
+           _selectionAreaRectTransform.gameObject.SetActive(true);
+            UpdateVisual();
         }
 
         private void UnitSelectionManager_OnSelectionAreaEnd(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            if(_selectionAreaRectTransform == null)
+                return;
+
+            // Disable the visual UI selector image when ending a drag selection.
+            _selectionAreaRectTransform.gameObject.SetActive(false);
+        }
+
+        private void UpdateVisual()
+        {
+            if(_selectionAreaRectTransform == null)
+                return;
+
+            Rect selectionAreaRect = UnitSelectionManager.Instance.GetSelectionAreaRect();
+
+            _selectionAreaRectTransform.anchoredPosition = new Vector2(selectionAreaRect.x, selectionAreaRect.y);
+            _selectionAreaRectTransform.sizeDelta = new Vector2(selectionAreaRect.width, selectionAreaRect.height);
         }
     }
 }
