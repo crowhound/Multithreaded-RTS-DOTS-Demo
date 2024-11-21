8d41a3c258a9fa7d7508c978a7cf528283703fee
crowhound <jonathan.westfall1@gmail.com>
2024-11-21 12:23:09 -0600

    Added a SearchRange value for the FindTarget data component.

--- Assets/Scripts/Authoring/FindTargetAuthoring.cs
+++ Assets/Scripts/Authoring/FindTargetAuthoring.cs
@@ -1,22 +1,24 @@
 using Unity.Entities;
 using UnityEngine;
 
-namespace SF
+namespace SF.EntitiesModule
 {
     public class FindTargetAuthoring : MonoBehaviour
     {
+        public float SearchRange;
+
         public class FindTargetAuthoringBaker : Baker<FindTargetAuthoring>
         {
             public override void Bake(FindTargetAuthoring authoring)
             {
                 Entity entity = GetEntity(TransformUsageFlags.Dynamic);
-                AddComponent(entity, new FindTarget());
+                AddComponent(entity, new FindTarget() { SearchRange = authoring.SearchRange});
             }
         }
     }
 
     public struct FindTarget : IComponentData
-    { 
-
+    {
+        public float SearchRange;
     }
 }
