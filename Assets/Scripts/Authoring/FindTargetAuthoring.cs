e238fc0a371d7e3b2fd7324943b2ba850cda1739
crowhound <jonathan.westfall1@gmail.com>
2024-11-21 14:18:38 -0600

    Updated the FindTarget ICOmponentData to have a timer support for certain actions.

--- Assets/Scripts/Authoring/FindTargetAuthoring.cs
+++ Assets/Scripts/Authoring/FindTargetAuthoring.cs
@@ -6,13 +6,20 @@ namespace SF.EntitiesModule
     public class FindTargetAuthoring : MonoBehaviour
     {
         public float SearchRange;
+        public FactionTypes TargetFaction;
+        public float TimerMax;
 
         public class FindTargetAuthoringBaker : Baker<FindTargetAuthoring>
         {
             public override void Bake(FindTargetAuthoring authoring)
             {
                 Entity entity = GetEntity(TransformUsageFlags.Dynamic);
-                AddComponent(entity, new FindTarget() { SearchRange = authoring.SearchRange});
+                AddComponent(entity, new FindTarget() 
+                {
+                    SearchRange = authoring.SearchRange,
+                    TargetFaction = authoring.TargetFaction,
+                    TimerMax = authoring.TimerMax,
+                });
             }
         }
     }
@@ -20,5 +27,8 @@ namespace SF.EntitiesModule
     public struct FindTarget : IComponentData
     {
         public float SearchRange;
+        public FactionTypes TargetFaction;
+        public float Timer;
+        public float TimerMax;
     }
 }
