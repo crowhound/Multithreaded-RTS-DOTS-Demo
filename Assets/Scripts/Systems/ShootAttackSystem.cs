bd2cfa662c5af064174d2b0723ea30f8f62cdf3e
crowhound <jonathan.westfall1@gmail.com>
2024-11-21 14:38:46 -0600

    Created a ShootAttack System to allow units to shoot at their target.

---
+++ Assets/Scripts/Systems/ShootAttackSystem.cs
@@ -0,0 +1,35 @@
+using Unity.Burst;
+using Unity.Entities;
+
+namespace SF.EntitiesModule
+{
+    partial struct ShootAttackSystem : ISystem
+    {
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+           foreach((
+                RefRW<ShootAttack> shootAttack, 
+                RefRO<Target> target) 
+                in SystemAPI.Query
+                    <RefRW<ShootAttack>, 
+                    RefRO<Target>>())
+            {
+                // If the TargetEntity is not null continue to next interation
+                if(target.ValueRO.TargetEntity != Entity.Null)
+                    continue;
+
+                shootAttack.ValueRW.Timer -= SystemAPI.Time.DeltaTime;
+
+                // If not enough time has passed to shoot go to next interation.
+                if(shootAttack.ValueRO.Timer > 0f)
+                    continue;
+
+                shootAttack.ValueRW.Timer = shootAttack.ValueRO.TimerMax;
+
+
+            } // End of Shoot Attack/Target foreach
+        }
+    }
+}
